7d013cd430b592ede36b283a2cb5be9f871a7f18
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-06 15:19:31 +0200

    affiche le dernier chiffre d'un nombre et indique sur supérieur ou inférieur à 5

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 if number < 0 :
-    last_digit = -(number % 10)
+    last_digit = -(abs(number) % 10)
 else :
     last_digit = number % 10
 print("Last digit of", number, "is", last_digit, end=' ')
