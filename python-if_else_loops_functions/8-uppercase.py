a2e2963165d8c99cd5933ace747fbe100ef0cbf0
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-07 00:32:23 +0200

    Transformation de minuscules en majuscules

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     if len(str) == 0:
-        print("")
         return
     for i in range(0, len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
