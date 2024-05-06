5456b823f5feeaf6f77717fb58d9d7a7ed5ea114
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-06 13:13:57 +0200

    print 3 times a string stored in the variable str, followed by its first 9 characters.

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 str = "Holberton School"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-for i in range (0, 3):
-    print(str, end='')
-print('\n', end='')
+print(str, end='')
+print(str, end='')
+print(str)
 print(str[:9])
