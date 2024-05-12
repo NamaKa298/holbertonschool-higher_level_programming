e9f7a48ab59b4debc7e5c778be4561d0e4e2f8fc
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-12 12:06:28 +0200

    ajout de l'ensemble du dossier

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for noms in dir(hidden_4):
+    names = dir(hidden_4)
+    for noms in names:
         if noms[:2] != '__':
             print(noms)
