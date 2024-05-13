5fb950ab1b11dc06e347c45cc5431b01ad04a902
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-13 15:53:33 +0200

    function that finds all multiples of 2 in a list

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    nouvelle_liste =list()
+    nouvelle_liste = list()
     for i in my_list:
         if i % 2:
             nouvelle_liste.append(False)
