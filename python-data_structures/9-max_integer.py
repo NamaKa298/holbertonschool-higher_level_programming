2f76507fec32135ad9812da4ccb0b918a6764e77
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-13 14:43:42 +0200

    function that finds the biggest integer of a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    valeur_max = 0
+    if len(my_list) == 0:
+        valeur_max = "None"
+        return valeur_max
+    else:
+        for i in my_list:
+            if i > valeur_max:
+                valeur_max = i
+        return valeur_max
