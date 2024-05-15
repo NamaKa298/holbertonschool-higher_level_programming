737184177d0aa3accc0324685c351b6f3ebcc2a5
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-15 11:22:14 +0200

    function that converts a Roman numeral to an integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string.isalpha() or roman_string == None:
+        return 0
+    dictionnaire_chiffre_romain={'I' : 1, 'X' : 10, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    valeur = 0
+    l =0
+    for i in range(0, len(roman_string)-1):
+        if dictionnaire_chiffre_romain[roman_string[i]] >= dictionnaire_chiffre_romain[roman_string[i+1]]:
+            valeur += dictionnaire_chiffre_romain[roman_string[i]]
+        else:
+            valeur -= dictionnaire_chiffre_romain[roman_string[i]]
+        l += 1
+    if dictionnaire_chiffre_romain[roman_string[l]] > dictionnaire_chiffre_romain[roman_string[l-1]]:
+        valeur += dictionnaire_chiffre_romain[roman_string[l]]
+    else:
+        valeur += dictionnaire_chiffre_romain[roman_string[l]]
+    return(valeur)
