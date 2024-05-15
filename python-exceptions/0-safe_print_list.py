d6b64c4f017256b602d4a504b6ef54c06f030178
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-15 14:44:35 +0200

    function that prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nombre_elements = 0
+    longueur = 0
+    for elements in my_list:
+        longueur += 1
+    try:
+        if x <= longueur:
+            for i in range(x):      
+                print(my_list[i], end="")
+                nombre_elements += 1
+            print("")
+        elif x > longueur:
+            for i in range(longueur):      
+                print(my_list[i], end="")
+                nombre_elements += 1
+            print("")
+    except IndexError:
+        pass
+    return nombre_elements
