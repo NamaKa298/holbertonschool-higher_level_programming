58a188adf51de1e5922a79b759b60335ea2ee42a
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-15 11:45:22 +0200

    function that converts a Roman numeral to an integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not roman_string.isalpha():
         return 0
-    if roman_string is None:
+    else if roman_string is None:
         return 0
     dict = {'I': 1, 'X': 10, 'V': 5, 'X': 10,
             'L': 50, 'C': 100, 'D': 500, 'M': 1000}
