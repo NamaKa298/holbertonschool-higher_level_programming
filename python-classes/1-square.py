abf2543d4a357b4b63edf3c2d662a88d1438d07b
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-16 15:42:43 +0200

    class Square that defines a square by

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+''' class Square that defines a square by '''
+
+
 class Square:
+    ''' class Square that defines a square by '''
     def __init__(self, size):
         self.__size = size
