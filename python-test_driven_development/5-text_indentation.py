a632ade95fb70d64bbaa2529b061353b74033520
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-20 21:29:38 +0200

    function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''4. Text indentation'''
+
+
+def text_indentation(text):
+    '''function that prints a text with 2 new lines after each of these characters: ., ? and :'''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if text[i].isalnum() or text[i] == ' ' or text[i] == '!':
+            print(text[i], end = '')
+        else:
+            print(text[i])
+            print()
+    print()
