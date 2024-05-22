f109526f654076f2d4be1ca0548f96f0ca1fc86a
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-22 11:42:23 +0200

    class MyList that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 '''1. My list'''
 
+
 class MyList(list):
+    """
+    Represent a custom list named my list
+    """
     
     def print_sorted(self):
         '''class MyList that inherits from list'''
