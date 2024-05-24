18acd61bcf576a363df5d864bb12c3618bac714c
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-24 11:23:59 +0200

    copie une str et supprime une lettre qui est à un certain indice

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    copie_de_str = str[:n] + str[n + 1:]
-    return copie_de_str
+    if n >= 0:
+        copie_de_str = str[:n] + str[n + 1:]
+        return copie_de_str
+    return str
