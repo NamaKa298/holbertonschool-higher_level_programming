8ba4716690abdf83edd8cfa842f34aadb5f17ea8
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-25 21:50:24 +0200

    calcul de la moyenne

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     coeff_tot = 0
     valeur_mult_par_coeff = 0
+    if list == []:
+        return 0
     for tup in my_list:
         coeff_tot += tup[1]
         valeur_mult_par_coeff += tup[0] * tup[1]
