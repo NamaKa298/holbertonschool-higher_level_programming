cb325f123c59b464e265bdb487ea44064aa8831e
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-29 15:26:53 +0200

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,9 +3,10 @@
 import requests
 import csv
 
+
 def fetch_and_print_posts():
     '''fetches all post from JSONPlaceholder'''
-    url_JSONPlaceholder=https://jsonplaceholder.typicode.com/
+    url_JSONPlaceholder = https: // jsonplaceholder.typicode.com/
     reponse = requests.get(url_JSONPlaceholder)
     print("Status code: {}".format(reponse.status_code))
     if reponse.status_code == 200:
@@ -13,9 +14,10 @@ def fetch_and_print_posts():
         for donnee in donnees_parsees:
             print(donnee.get('title'))
 
+
 def fetch_and_save_posts():
     '''fetches all post from JSONPlaceholder'''
-    url_JSONPlaceholder=https://jsonplaceholder.typicode.com/
+    url_JSONPlaceholder = https: // jsonplaceholder.typicode.com/
     reponse = requests.get(url_JSONPlaceholder)
     if reponse.status_code == 200:
         donnees_parsees = reponse.json()
