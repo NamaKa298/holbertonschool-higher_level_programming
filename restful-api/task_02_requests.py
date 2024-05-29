0d8e7de5eaa5e9547fcf9182af0391a4d4452409
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-29 22:01:53 +0200

    2. Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,19 +8,19 @@ import requests
 
 def fetch_and_print_posts():
     '''fetches all post from JSONPlaceholder'''
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print("Status Code: {}".format(response.status_code))
-    if response.status_code == 200:
-        posts = response.json()
+    reponse = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("Status Code: {}".format(reponse.status_code))
+    if reponse.status_code == 200:
+        posts = reponse.json()
         for post in posts:
             print(post['title'])
 
 
 def fetch_and_save_posts():
     '''fetches all post from JSONPlaceholder'''
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if response.status_code == 200:
-        posts = response.json()
+    reponse = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if reponse.status_code == 200:
+        posts = reponse.json()
         posts_list = [{"id": post["id"], "title": post["title"],
                        "body": post["body"]} for post in posts]
         with open('posts.csv', 'w', newline='') as csvfile:
