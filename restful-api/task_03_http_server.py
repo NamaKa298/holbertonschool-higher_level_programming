0a15bad6381016c98010a74b95a9f0553c8eaab8
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-29 23:08:16 +0200

    3. Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,6 +5,7 @@
 import http.server
 import json
 import socketserver
+from http import HTTPStatus
 
 
 class Handler(http.server.BaseHTTPRequestHandler):
@@ -16,7 +17,23 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
-            
+        elif self.path == '/':
+            self.send_response(HTTPStatus.OK)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/status':
+            self.send_response(HTTPStatus.OK)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            status = {"status": "OK"}
+            self.wfile.write(json.dumps(status).encode('utf-8'))
+        else:
+            self.send_response(HTTPStatus.NOT_FOUND)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_message = {"error": "Endpoint not found"}
+            self.wfile.write(json.dumps(error_message).encode('utf-8'))
 
 PORT = 8080
 
