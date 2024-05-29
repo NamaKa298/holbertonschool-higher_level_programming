ec2fbb5e25b99564daea1ae6dc21eff642520b44
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-29 22:58:32 +0200

    3. Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,36 +4,22 @@
 
 import http.server
 import json
-from http import HTTPStatus
+import socketserver
 
-class GestionnaireDeRequetesSimple(http.server.BaseHTTPRequestHandler):
+
+class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
+        '''handles GET requests'''
+        if self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(HTTPStatus.OK)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Bonjour, ceci est une API simple !")
-        elif self.path == '/data':
-            self.send_response(HTTPStatus.OK)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
-        elif self.path == '/status':
-            self.send_response(HTTPStatus.OK)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            status = {"status": "OK"}
-            self.wfile.write(json.dumps(status).encode('utf-8'))
-        else:
-            self.send_response(HTTPStatus.NOT_FOUND)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            error_message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode('utf-8'))
+            
 
-# Configuration et lancement du serveur
-adresse_serveur = ('', 8000)
-httpd = http.server.HTTPServer(adresse_serveur, GestionnaireDeRequetesSimple)
-print("Démarrage du serveur HTTP sur le port 8000")
-httpd.serve_forever()
+PORT = 8080
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
