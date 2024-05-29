58227ee0e2d63a2c74b83857eb4d4b5955306ccf
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-29 15:28:09 +0200

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -19,11 +19,11 @@ def fetch_and_save_posts():
     '''fetches all post from JSONPlaceholder'''
     url_JSONPlaceholder = https: // jsonplaceholder.typicode.com/
     reponse = requests.get(url_JSONPlaceholder)
+    print("Status code: {}".format(reponse.status_code))
     if reponse.status_code == 200:
         donnees_parsees = reponse.json()
-        with open('posts.csv', 'w', newline='') as fichier:
-            csvwriter = csv.writer(fichier)
+        with open('posts.csv', 'w', newline='') as fichier_csv:
+            csv_writer = csv.writer(fichier_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for donnee in donnees_parsees:
-                csvwriter.writerow([donnee.get('userId'), donnee.get('id'),
-                                    donnee.get('title')])
-    print("Saved the posts to posts.csv")
+                csv_writer.writerow([donnee.get('userId'), donnee.get('id'), donnee.get('title')])
+    print("Données enregistrées dans posts.csv")
