0eb9d76d5ceabf29fdc128b0e955896b3d431887
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-30 15:06:47 +0200

    4. Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,13 +3,22 @@
 
 
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
-users = {
-    "jane": {"username": "jane", "name": "Jane",
-             "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John",
-             "age": 30, "city": "New York"}
+users ={
+    "jane":
+    {"username": "jane",
+     "name": "Jane",
+     "age": 28,
+     "city": "Los Angeles"
+    },
+    "john":
+    {"username": "john",
+     "name": "John",
+     "age": 30,
+     "city": "New York"
+    }
 }
 
 
@@ -29,12 +38,9 @@ def status():
 
 
 @app.route('/users/<username>', methods=['GET'])
-def get_user(username):
-    user = users.get(username)
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({"error": "User not found"}), 404
+def get_users(username):
+    if username in users:
+        return jsonify(users[username])
 
 
 @app.route('/add_user', methods=['POST'])
@@ -43,10 +49,9 @@ def add_user():
     username = user_data.get("username")
     if not username or username in users:
         return jsonify({"error": "Invalid or duplicate username"}), 400
-
     users[username] = user_data
     return jsonify({"message": "User added", "user": user_data}), 201
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run()
