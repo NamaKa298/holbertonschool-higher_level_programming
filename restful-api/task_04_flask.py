cfe00ab18447c402b0eddeae1a1bfce9d90c6264
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-30 16:49:11 +0200

    4. Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -4,6 +4,7 @@
 
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 users = {
@@ -29,17 +30,17 @@ def home():
     return "Welcome to the Flask API!"
 
 
-@app.route('/data', methods=['GET'])
+@app.route('/data')
 def get_data():
     return jsonify(list(users.keys()))
 
 
-@app.route('/status', methods=['GET'])
+@app.route('/status')
 def status():
     return "OK"
 
 
-@app.route('/users/<username>', methods=['GET'])
+@app.route('/users/<username>')
 def get_users(username):
     if username in users:
         return jsonify(users[username])
@@ -49,10 +50,10 @@ def get_users(username):
 def add_user():
     user_data = request.get_json()
     username = user_data.get("username")
-    if not username or username in users:
-        return jsonify({"error": "Invalid or duplicate username"}), 400
+    if username in users:  # Check if username exists
+        return jsonify({"error": "L'utilisateur existe déjà"})
     users[username] = user_data
-    return jsonify({"message": "User added", "user": user_data}), 201
+    return jsonify({"message": "User added", "user": user_data})
 
 
 if __name__ == "__main__":
