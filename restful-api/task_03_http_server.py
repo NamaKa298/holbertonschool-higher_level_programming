84824b1fc38e9adb4f69dc364a2c49f9d03d9bc5
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-31 12:02:23 +0200

    task3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -25,8 +25,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            status = {"status": "OK"}
-            self.wfile.write(json.dumps(status).encode('utf-8'))
+            self.wfile.write(b'OK')
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
