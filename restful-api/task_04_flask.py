1bb66114925be2c04f57c66c6e6d65f7b3ec9e8b
NamaKa298 <marion.saint-martin@protonmail.com>
2024-05-31 12:10:01 +0200

    task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -24,23 +24,28 @@ def get_data():
 def status():
     return "OK"
 
-
 @app.route('/users/<username>')
 def get_users(username):
     if username in users:
         return jsonify(users[username])
+    return jsonify({"error": "User not found"}), 404
 
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    user_data = request.get_json()
-    username = user_data.get("username")
-    if not username:
-        return jsonify({"User not found"}), 400
-    if username in users:  # Check if username exists
-        return jsonify({"error": "L'utilisateur existe déjà"})
-    users[username] = user_data
-    return jsonify({"message": "User added", "user": user_data})
+    if not request.json or "username" not in request.json:
+        return jsonify({"error": "Username is required"}), 400
+    user_data = request.json
+    username = user_data["username"]
+    # if username in users:
+    #     return jsonify({"error": "User already exists"}), 400
+    users[username] = {
+        "username": user_data.get("username"),
+        "name": user_data.get("name"),
+        "age": user_data.get("age"),
+        "city": user_data.get("city")
+    }
+    return jsonify({"message": "User added", "user": users[username]}), 201
 
 
 if __name__ == "__main__":
