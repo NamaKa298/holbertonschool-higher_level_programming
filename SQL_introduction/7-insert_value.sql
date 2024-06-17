3e53752186f5358f1b2a39509b6e10b580c3a15b
NamaKa298 <marion.saint-martin@protonmail.com>
2024-06-17 22:23:41 +0200

    script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,6 @@
 -- ================================================================================================
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
 -- ================================================================================================
-INSERT INTO first_table(id, name)
-  VALUES ('89', '562-985-4671', 'Best School');
+ALTER table first_table,
+ADD id varchar(89),
+ADD name varchar(Best School);
