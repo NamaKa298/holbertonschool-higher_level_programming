3b64d4bf563c7d817e26cd5e0f615052eace2a93
NamaKa298 <marion.saint-martin@protonmail.com>
2024-06-18 11:46:31 +0200

    script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- ===================================================
 -- script that creates the MySQL server user user_0d_1
 -- ===================================================
-CREATE USER 'user_0d_1' IF NOT EXISTS WITH PASSWORD 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1' WITH PASSWORD 'user_0d_1_pwd';
