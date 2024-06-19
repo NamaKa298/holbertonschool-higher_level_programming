ce2633d424b87bf603ee95324702c186dfc6b10c
NamaKa298 <marion.saint-martin@protonmail.com>
2024-06-19 22:28:02 +0200

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""
-Module that display all values in the states table
-of a database where name matches the argument.
-"""
-
+"""script that takes in an argument and displays all values\\
+    in the states table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
-    database = MySQLdb.connect(
-        host="localhost",
-        port=3306,
+    db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
-        database=sys.argv[3]
+        db=sys.argv[3],
+        port=3306
     )
+    
+    cur = db.cursor()
 
-    cursor = database.cursor()
-
-    cursor.execute("""SELECT * FROM states
-                      WHERE name LIKE BINARY '{}'
-                      ORDER BY states.id
-                   """.format(sys.argv[4]))
-
-    for row in cursor.fetchall():
-        print(row)
-
-    cursor.close()
-    database.close()
+    cur.execute("SELECT states.id, states.name FROM states WHERE\
+                states.name = %s ORDER BY states.id\
+                ASC", (sys.argv[4],))
+    for i in cur:
+        print(i)
