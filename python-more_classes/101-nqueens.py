48562eb9acffde3109949d33dc99490041057a6d
NamaKa298 <marion.saint-martin@protonmail.com>
2024-06-23 10:34:36 +0200

    N queens

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,35 @@
+import sys
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens(board, col, solutions):
+    if col >= len(board):
+        solution = []
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return True
+
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1, solutions) or res
+            board[i][col] = 0
+
+    return res
