43480e05fb4c6434d2c1ace6e754884f25bb1521
NamaKa298 <marion.saint-martin@protonmail.com>
2024-06-23 22:32:19 +0200

    13. Can I?

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 '''13. Can I ?'''
+
+
 def add_attribute(obj, attr_name, attr_value):
+    '''fonction qui ajoute un nouvel attribut'''
     if hasattr(obj, '__dict__'):
         setattr(obj, attr_name, attr_value)
     else:
