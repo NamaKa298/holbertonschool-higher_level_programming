8636e1b13624debdfaef33ad2784a47c5f2843a8
NamaKa298 <marion.saint-martin@protonmail.com>
2024-07-16 09:39:15 +0200

    0. Creating a Simple Templating Program

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,10 +1,12 @@
 import os
 
+
 def generate_invitations(template_content, attendees):
     if not isinstance(template_content, str):
         print("Erreur : template n'est pas dans le bon format.")
         return
-    if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
+    if not isinstance(attendees, list)
+    or not all(isinstance(attendee, dict) for attendee in attendees):
         print("Erreur : attendees n'est pas dans le bon format.")
         return
     if not template_content:
@@ -13,19 +15,22 @@ def generate_invitations(template_content, attendees):
     if not attendees:
         print("No data provided, no output files generated.")
         return
-    
     for index, attendee in enumerate(attendees, start=1):
         invitation = template_content
         try:
-            for placeholder, value in attendee.items():
-                invitation = invitation.replace("{{{}}}".format(placeholder), value if value is not None else "N/A")
-            
+            invitation = invitation.replace(
+                "{name}", attendee.get("name", "N/A"))
+            invitation = invitation.replace("{event_title}",
+                                            attendee.get("event_title", "N/A"))
+            invitation = invitation.replace("{event_date}",
+                                            attendee.get("event_date", "N/A"))
+            invitation = invitation.replace(
+                "{event_location}", attendee.get("event_location", "N/A"))
             output_filename = "output_{}.txt".format(index)
-            if os.path.exists(output_filename):
-                print("Le fichier {} existe déjà, veuillez vérifier.".format(output_filename))
-                continue
-            
             with open(output_filename, "w", encoding="utf-8") as file:
                 file.write(invitation)
+            print("Fichier généré : {}".format(output_filename))
         except Exception as e:
-            print("Une erreur est survenue lors de la génération de l'invitation pour {}: {}".format(attendee.get('name', 'N/A'), e))
+            print("Une erreur est survenue lors\
+                de la génération de l'invitation pour {}: {}"
+                  .format(attendee.get('name', 'N/A'), e))
