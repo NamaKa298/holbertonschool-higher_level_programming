a707613f22ed7d0dd86a855a1be602abeef2d953
NamaKa298 <marion.saint-martin@protonmail.com>
2024-07-17 10:45:48 +0200

    task 3

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -33,18 +33,20 @@ def items():
 def products():
     source = request.args.get('source')
     id = request.args.get('id', type=int)
-    if source == 'json':
-        data = read_json('products.json')
-    elif source == 'csv':
-        data = read_csv('products.csv')
-    else:
-        return render_template('product_display.html', error="Wrong source")
-
-    if id:
-        data = [product for product in data if product['id'] == id]
-        if not data:
-            return render_template('product_display.html', error="Product not found")
-    return render_template('product_display.html', products=data)
+    try:
+        if source == 'json':
+            data = read_json('products.json')
+        elif source == 'csv':
+            data = read_csv('products.csv')
+        elif source not in ['json', 'csv']:
+            return render_template('product_display.html', error="Wrong source")
+        if id:
+            data = [product for product in data if product['id'] == id]
+            if not data:
+                return render_template('product_display.html', error="Product not found")
+        return render_template('product_display.html', products=data)
+    except Exception as e:
+        return render_template('product_display.html', error=str(e))
 
 def read_json(file_path):
     with open(file_path, 'r') as f:
@@ -55,6 +57,8 @@ def read_csv(file_path):
     with open(file_path, newline='') as f:
         reader = csv.DictReader(f)
         for row in reader:
+            row['id'] = int(row['id'])
+            row['price'] = float(row['price'])
             products.append(row)
     return products
 
