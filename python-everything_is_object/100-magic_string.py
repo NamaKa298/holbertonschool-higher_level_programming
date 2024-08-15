291883da9f524685907f9ab0c0118cd55fad09c8
NamaKa298 <marion.saint-martin@protonmail.com>
2024-08-15 12:43:20 +0200

    30. #pythonic

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def magic_string():
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 0
-    magic_string.count += 1
-    return ", ".join(["BestSchool"] * magic_string.count)
+    return ", ".join(["BestSchool"] * 4)
