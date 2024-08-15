bc551a83ea1c974b1abf62f9162075355cb802f4
NamaKa298 <marion.saint-martin@protonmail.com>
2024-08-15 12:46:22 +0200

    30. #pythonic

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string():
-    return ", ".join(["BestSchool"] * 4)
+def magic_string(magic_string=[]): 
+    magic_string += ["BestSchool"]
+    return ", ".join(magic_string)
